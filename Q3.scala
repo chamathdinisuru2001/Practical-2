215ca6488a70a210d981a00381c690b6a900f799
Chamath <chamathdinisuru@gmail.com>
2024-06-25 08:44:43 +0530

    first commit

--- Q3.scala
+++ Q3.scala
@@ -11,7 +11,7 @@ object Q3 extends App{
 
     def tax(m:Double):Double=m*0.12
 
-    def takeHomeSal(x:Int,y:Int):Double= grossSal(x,y)+tax(grossSal(x,y))
+    def takeHomeSal(x:Int,y:Int):Double= grossSal(x,y)-tax(grossSal(x,y))
 
     val hoursNormal = 40
     val hoursOT = 30
